7d69619c8f2a4b5a68bec65cfcfbbde2805d6f30
Emanuel F. Oliveira <emfolg@gmail.com>
2016-03-27 02:13:24 -0300

    parser interface continuation...

--- src/parser.c
+++ src/parser.c
@@ -1,23 +1,46 @@
 #include <stdlib.h>
 #include "parser.h"
 
+/*
+ * Macros
+ */
+
+#define PARSER_BUFFER_SIZE 12000UL
+
+/*
+ * Implementations
+ */
+
 parser_context_ref parser_create_context(void)
 {
-    parser_context_ref ctx = (parser_context_ref)malloc(sizeof(parser_context_t) + PARSER_BUFFER_SIZE);
-    if ( ctx != NULL ) {
-        ctx->in_base = (parser_index_t)ctx + sizeof(parser_context_t);
-        ctx->out_limit = (parser_index_t)ctx + sizeof(parser_context_t) + PARSER_BUFFER_SIZE - 1;
-        ctx->out_base = (parser_index_t)ctx->in_base + (PARSER_BUFFER_SIZE / 2);
-        ctx->in_limit = (parser_index_t)ctx->out_base - 1;
-        ctx->in_ptr = ctx->in_base;
-        ctx->out_ptr = ctx->out_base;
-        ctx->status = 0L;
+    parser_context_ref c = (parser_context_ref)malloc(sizeof(parser_context_t) + PARSER_BUFFER_SIZE);
+    if ( c != NULL ) {
+        c->in_base = (parser_index_t)c + sizeof(parser_context_t);
+        c->out_limit = (parser_index_t)c + sizeof(parser_context_t) + PARSER_BUFFER_SIZE - 1;
+        c->out_base = (parser_index_t)c->in_base + (PARSER_BUFFER_SIZE / 2);
+        c->in_limit = (parser_index_t)c->out_base - 1;
+        c->in_ptr = c->in_base;
+        c->out_ptr = c->out_base;
+        c->status = 0UL;
     }
-    return ctx;
+    return c;
 }
 
-void parser_destroy_context(parser_context_ref context)
+void parser_destroy_context(parser_context_ref c)
 {
-    free(context);
+    free(c);
 }
 
+parser_reg_t parser_verify_context_integrity(parser_context_ref c)
+{
+    parser_reg_t result = 0UL;
+    if (c->in_base - (parser_index_t)c != sizeof(parser_context_t))
+        result |= (1 << 0);
+    if (c->out_limit + 1 - c->in_base != PARSER_BUFFER_SIZE)
+        result |= (1 << 1);
+    if (c->out_base != c->in_limit + 1)
+        result |= (1 << 2);
+    if (c->in_limit <= c->in_base)
+        result |= (1 << 3);
+    return result;
+}
--- src/parser.h
+++ src/parser.h
@@ -2,12 +2,6 @@
 #define __PARSER_H
 
 /*
- * Macros
- */
-
-#define PARSER_BUFFER_SIZE ( 1 * 4096UL )
-
-/*
  * Type Definitions
  */
 
@@ -15,9 +9,15 @@ typedef unsigned long parser_reg_t;
 typedef unsigned char parser_char_t;
 typedef parser_char_t *parser_index_t;
 typedef struct parser_context {
-    parser_index_t in_base, in_limit, in_ptr;
-    parser_index_t out_base, out_limit, out_ptr;
     parser_reg_t status;
+    /* input */
+    parser_index_t in_base;   /* first input address */
+    parser_index_t in_limit;  /* last input address */
+    parser_index_t in_ptr;    /* next available position */
+    /* output */
+    parser_index_t out_base;  /* first output address */
+    parser_index_t out_limit; /* last output address */
+    parser_index_t out_ptr;   /* next available address */
 } parser_context_t;
 typedef parser_context_t *parser_context_ref;
 
@@ -30,5 +30,8 @@ parser_context_ref parser_create_context(void);
 
 /* Dealloc Parsing context structure... */
 void parser_destroy_context(parser_context_ref context);
+
+/* Verify context integrity... */
+parser_reg_t parser_verify_context_integrity(parser_context_ref context);
 
 #endif
