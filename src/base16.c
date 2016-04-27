68c377ab10ed6d449652541c912833cfca9ccfb1
Emanuel F. Oliveira <emfolg@gmail.com>
2016-04-27 00:37:50 -0300

    Continuation...

--- src/base16.c
+++ src/base16.c
@@ -11,6 +11,10 @@
     #error "BAD VALUE FOR BASE16_BUFSIZ"
 #endif
 
+#define CHAR_COMMENT '#'
+#define IS_CHAR_COMMENT(c) \
+    ((c) == CHAR_COMMENT)
+
 #define IS_CHAR_EOL(c) \
     ((c) == '\n')
 
@@ -29,12 +33,12 @@
 #define ADJUST_HEXDIGIT(c) \
     ((c) >= 'A' && (c) <= 'F' && ((c) |= 0x20))
 
-#define GET_HEXDIGIT(c) \
-    (((c) >= 'a' && (c) <= 'f') ? ((c) - 'a' + 0x0a) : ((c) - '0'))
+#define ENCODE_HEXDIGIT(c) \
+    ((((c) >= 'a' && (c) <= 'f') ? ((c) - 'a' + 0x0a) : ((c) - '0')) & 0x0f)
 
 #define CTRL_MASK_MODE   (0x03 << 0)
 #define CTRL_MASK_STATUS (0x03 << 2)
-#define CTRL_MASK_ERROR  (0x03 << 4)
+#define CTRL_MASK_ERROR  (0x07 << 4)
 
 #define CTRL_MODE_NONE   (0x00 << 0)
 #define CTRL_MODE_ENCODE (0x01 << 0)
@@ -46,18 +50,29 @@
 #define CTRL_STATUS_ERROR   (0x03 << 2)
 
 #define CTRL_ERROR_NONE  (0x00 << 4)
+#define CTRL_ERROR_MODE  (0x01 << 4)
+#define CTRL_ERROR_FRMT  (0x01 << 4)
 #define CTRL_ERROR_INVAL (0x01 << 4)
-#define CTRL_ERROR_ILSEQ (0x02 << 4)
-#define CTRL_ERROR_NOBUF (0x03 << 4)
+#define CTRL_ERROR_ILSEQ (0x01 << 4)
+#define CTRL_ERROR_NOBUF (0x01 << 4)
 
 #define IS_MODE(m, v) \
     (((m) & CTRL_MASK_MODE) == (v))
+
+#define SET_MODE(m, v) \
+    ((m) = ((m) & ~CTRL_MASK_MODE) | ((v) & CTRL_MASK_MODE))
 
 #define IS_STATUS(s, v) \
     (((s) & CTRL_MASK_STATUS) == (v))
 
 #define SET_STATUS(s, v) \
-    ((s) = ((s) & ~CTRL_MASK_STATUS) | (v))
+    ((s) = ((s) & ~CTRL_MASK_STATUS) | ((v) & CTRL_MASK_STATUS))
+
+#define GET_ERROR_CODE(e) \
+    (((e) & CTRL_MASK_ERROR) >> 4)
+
+#define SET_ERROR(e, v) \
+    ((e) = ((e) & ~CTRL_MASK_ERROR) | ((v) & CTRL_MASK_ERROR))
 
 /*
  * Implementations
@@ -72,16 +87,16 @@ base16_context_ref base16_create_context(void)
         c->out_limit = c->in_base + (BASE16_BUFSIZ - 1);
         c->out_base  = c->out_limit - ((BASE16_BUFSIZ / 2) - 1); /* default ratio... 1/2 + 1/2 */
         c->in_limit  = c->out_base - 1;
+        c->in_first  = NULL;
         c->in_last   = NULL;
-        c->in_ptr    = NULL;
+        c->out_first = NULL;
         c->out_last  = NULL;
-        c->out_ptr   = NULL;
         c->char_cnt  = 0;
         c->line_cnt  = 0;
-        c->line_ptr  = 0;
+        c->line_cur  = 0;
         c->ctrl      = CTRL_MODE_NONE | CTRL_STATUS_OK | CTRL_ERROR_NONE;
-        for (i = 0; i < (base16_reg_t)sizeof(base16_buffer_t); i++)
-            c->buf[i] = '\0';
+        for (i = 0; i < (base16_reg_t)sizeof(base16_cache_t); i++)
+            c->cache[i] = '\0';
     }
     return c;
 }
@@ -112,44 +127,51 @@ base16_reg_t base16_encode(base16_context_ref c)
 
     base16_index_t i_ptr, i_lim, o_ptr, o_lim;
     base16_char_t i_chr, o_chr;
-    base16_reg_t cr;
+    base16_reg_t ctrl;
 
-    cr = c->ctrl;
-    if (IS_MODE(cr, CTRL_MODE_ENCODE))
-        return 1;
+    ctrl = c->ctrl;
+    if (!IS_MODE(ctrl, CTRL_MODE_ENCODE)) {
+        SET_ERROR(ctrl, CTRL_ERROR_MODE);
+        goto label_quit;
+    }
 
-    i_ptr = c->in_base;
-    i_lim = c->in_ptr;
-    o_ptr = c->out_base;
-    o_lim = c->out_limit;
+    /* reset error data */
+    SET_ERROR(ctrl, CTRL_ERROR_NONE);
 
+    i_ptr = c->in_first;
+    i_lim = c->in_last;
+    o_ptr = c->out_first;
+    o_lim = c->out_last;
+
+    /* main loop */
     while (i_ptr <= i_lim && o_ptr <= o_lim) {
         i_chr = *i_ptr++;
         ADJUST_HEXDIGIT(i_chr);
-        if (IS_STATUS(cr, CTRL_STATUS_PAIR)) {
+        if (IS_STATUS(ctrl, CTRL_STATUS_PAIR)) {
             if (IS_CHAR_HEXDIGIT(i_chr)) {
-                SET_STATUS(cr, CTRL_STATUS_OK);
-                o_chr = (o_chr << 4) | GET_HEXDIGIT(i_chr);
+                SET_STATUS(ctrl, CTRL_STATUS_OK);
+                o_chr = (o_chr << 4) | ENCODE_HEXDIGIT(i_chr);
                 *o_ptr++ = o_chr;
             } else {
-                SET_STATUS(cr, CTRL_STATUS_ERROR);
-                /* TODO: SET_ERROR(cr, CTRL_ERROR_ILSEQ)
+                SET_STATUS(ctrl, CTRL_STATUS_ERROR);
+                SET_ERROR(ctrl, CTRL_ERROR_ILSEQ);
+                /* TODO: SET_ERROR(ctrl, CTRL_ERROR_ILSEQ)
                  * and update statistics...
                  */
                 break;
             }
-        } else if (IS_STATUS(cr, CTRL_STATUS_COMMENT)) {
+        } else if (IS_STATUS(ctrl, CTRL_STATUS_COMMENT)) {
             if (IS_CHAR_EOL(i_chr)) {
-                SET_STATUS(cr, CTRL_STATUS_COMMENT);
+                SET_STATUS(ctrl, CTRL_STATUS_OK);
             }
             continue;
         }
     }
 
-    c->ctrl = cr;
-    c->out_ptr = o_ptr;
+label_quit:
+    c->ctrl = ctrl;
 
-    return 0;
+    return GET_ERROR_CODE(ctrl);
 
 }
 
--- src/base16.h
+++ src/base16.h
@@ -14,24 +14,24 @@
 typedef unsigned long base16_reg_t;
 typedef unsigned char base16_char_t;
 typedef base16_char_t *base16_index_t;
-typedef base16_char_t base16_buffer_t[8];
+typedef base16_char_t base16_cache_t[8];
 typedef struct base16_context {
     base16_reg_t ctrl;
     base16_reg_t frmt;
     base16_reg_t char_cnt;
     base16_reg_t line_cnt;
-    base16_reg_t line_ptr;
+    base16_reg_t line_cur;
     /* input */
     base16_index_t in_base;   /* address of first available char */
     base16_index_t in_limit;  /* address of last available char */
+    base16_index_t in_first;  /* address of first valid char */
     base16_index_t in_last;   /* address of last valid char */
-    base16_index_t in_ptr;    /* auxiliary pointer */
     /* output */
     base16_index_t out_base;  /* address of first available char */
     base16_index_t out_limit; /* address of last available char */
+    base16_index_t out_first; /* address of first valid char */
     base16_index_t out_last;  /* address of last valid char */
-    base16_index_t out_ptr;   /* auxiliary pointer */
-    base16_buffer_t buf;
+    base16_cache_t cache;
 } base16_context_t;
 typedef base16_context_t *base16_context_ref;
 
